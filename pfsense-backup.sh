7eb1334ecc10655684d33caf983ac27711cb0fbe
Dan <email@danielhand.io>
2018-02-04 21:19:16 +0000

    Updated to allow backup of multiple hosts

--- pfsense-backup.sh
+++ pfsense-backup.sh
@@ -4,12 +4,19 @@
 # Created:   04/02/2018
 
 DATE=`date "+%d-%m-%Y-%H:%M"`
-HOST=hostname
+HOSTS="10.50.3.1 rtr0.example.local"
 USER=root
 BACKUPPATH=/pfsense-backup
+
 
 ### Delete configuration files older than 30 days
 find $BACKUPPATH -type f -name '*.xml' -mtime +30 -exec rm {} \;
 
+for HOST in $HOSTS
+
+do
+
 ### Copy config.xml from host with date and timestamp
 scp $USER@$HOST:/cf/conf/config.xml $BACKUPPATH/$HOST-$DATE.xml
+
+done
